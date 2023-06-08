f9f5751efea26d1c59d344b48cc90fe022bf4954
Roy P A <roy@nintrivalabs.com>
2023-06-08 14:08:09 +0530

    some1

--- background.js
+++ background.js
@@ -19,3 +19,16 @@ chrome.action.onClicked.addListener(function(tab) {
 //     // });
 //   }
 // });
+
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//   if (request.message === "data_extracted") {
+
+//     console.log("Entered IF Block");
+//     // const data = request.data;
+//     // console.log("Extracted data:", data);
+//     // chrome.action.setPopup({ tabId: sender.tab.id, popup: "popup.html" });
+//     // chrome.action.getPopup({ tabId: sender.tab.id }, function(popup) {
+//     //   chrome.tabs.sendMessage(sender.tab.id, { message: "update_popup", textContent: request.textContent, popup: popup });
+//     // });
+//   }
+// });
--- contentScript.js
+++ contentScript.js
@@ -1,10 +1,10 @@
 // contentScript.js
 
-// Helper function to download an image
+
 function downloadImage(url, filename) {
   fetch(url)
-    .then(response => response.blob())
-    .then(blob => {
+    .then((response) => response.blob())
+    .then((blob) => {
       const url = URL.createObjectURL(blob);
       const link = document.createElement("a");
       link.href = url;
@@ -38,10 +38,42 @@ function downloadText(content, filename) {
 // Scrape images and text
 function scrapeData() {
   // console.log("Hello");
-  const images = Array.from(document.querySelectorAll("img")).map(image => image.src);
-  const text = document.querySelector("body").innerText;
+  // const images = Array.from(document.querySelectorAll("img")).map(
+  //   (image) => image.src
+  // );
+  //const text = document.querySelector("body").innerText;
 
-   
+  const text = "Hello1";
+
+  var headingName ="";
+  if(document.querySelector(".item__name").innerText){
+    headingName =  "Heading:" +  document.querySelector(".item__name").innerText;
+  }else{
+    headingName = "";
+  }
+  var condition ="";
+  if(document.querySelector(".item__name").innerText){
+    condition =  "Heading:" +  document.querySelector(".item__name").innerText;
+  }else{
+    condition = "";
+  }
+  var age ="";
+  if(document.querySelector(".item__name").innerText){
+    age =  "Heading:" +  document.querySelector(".item__name").innerText;
+  }else{
+    age = "";
+  }
+  var description ="";
+  if(document.querySelector(".item__name").innerText){
+    description =  "Heading:" +  document.querySelector(".item__name").innerText;
+  }else{
+    description = "";
+  }
+  
+
+
+
+
 
   // Download images
   // images.forEach((imageUrl, index) => {
@@ -51,20 +83,24 @@ function scrapeData() {
 
   // Download text content
   const textFilename = "text_content.txt";
-  downloadText(text, textFilename);
+  downloadText(headingName, textFilename);
 
-  var headingName = document.querySelector("item_name").innerText;
-  const data = {
-    title: 'hello',
-   
-    // Add more data points as needed
-  };
-  chrome.runtime.sendMessage({ message: "data_extracted", data });
- 
+  // var headingName = document.querySelector("item_name").innerText;
+  // const data = {
+  //   title: "hello",
+
+  //   // Add more data points as needed
+  // };
+  //chrome.runtime.sendMessage({ message: "data_extracted", data });
+
+  // chrome.runtime.sendMessage(
+  //   { Message: "data_extracted" },
+  //   function (response) {}
+  // );
 }
 
 // Listen for messages from the background script
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.message === "scrape_data") {
     scrapeData();
   }
