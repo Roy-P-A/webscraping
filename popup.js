c69f9d49bf091e994c30df9a06c7e9de1fc797d8
Roy P A <roy@nintrivalabs.com>
2023-06-09 18:23:58 +0530

    superq

--- popup.js
+++ popup.js
@@ -1,15 +1,15 @@
 // popup.js
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const scrapeButton = document.getElementById("scrapeButton");
-  scrapeButton.addEventListener("click", function() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+  scrapeButton.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, { message: "scrape_data" });
     });
   });
 });
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.message === "update_popup") {
     // Handle the message to update the DOM in the popup.html
 
@@ -18,6 +18,10 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
     // Update the DOM of the popup.html
     const dataContainer = document.getElementById("data-container");
-    dataContainer.textContent = receivedData;
+    if (receivedData == "") {
+    } else {
+      dataContainer.textContent = receivedData;
+      dataContainer.classList.add("data-container1");
+    }
   }
 });
