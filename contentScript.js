dd8e147c70e078934c1759e2ee8998f82bdcee3f
Roy P A <roy@nintrivalabs.com>
2023-06-26 16:29:04 +0530

    some change

--- contentScript.js
+++ contentScript.js
@@ -14,20 +14,49 @@ function wait(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+function sendPostRequest(url, payload) {
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => {
+      if (response.ok) {
+        // Request successful, handle response
+        return response.json();
+      } else {
+        // Request failed, handle error
+        throw new Error("Error: " + response.status);
+      }
+    })
+    .then((responseData) => {
+      // Handle the response data
+      console.log(responseData);
+      const data1 = "Success";
+      chrome.runtime.sendMessage({ message: "data_extracted", data: data1 });
+    })
+    .catch((error) => {
+      // Handle any errors
+      console.error("Error:", error);
+      const data1 = "Failed";
+      chrome.runtime.sendMessage({ message: "data_extracted", data: data1 });
+    });
+}
+
 // Scrape images and text
 async function scrapeData() {
-
-
   var item = "";
   if (document.querySelector(".categories")) {
-    item = "item: " + document.querySelector(".categories").innerText;
+    item = document.querySelector(".categories").innerText;
   } else {
     item = "";
   }
 
   var headingName = "";
   if (document.querySelector(".item__name")) {
-    headingName = "Heading: " + document.querySelector(".item__name").innerText;
+    headingName = document.querySelector(".item__name").innerText;
   } else {
     headingName = "";
   }
@@ -45,10 +74,9 @@ async function scrapeData() {
   }
   var description = "";
   if (document.querySelector(".item__description")) {
-    description =
-      "Description: " + document.querySelector(".item__description").innerText;
+    description = document.querySelector(".item__description").innerText;
   } else {
-    description = "Description : ";
+    description = "";
   }
   var rate = "";
   if (document.querySelector(".item-rate__sum")) {
@@ -60,9 +88,9 @@ async function scrapeData() {
   var lenderName = "";
   if (document.querySelector(".item-lender__name")) {
     lenderName =
-      "lender Name: " + document.querySelector(".item-lender__name").innerText;
+      document.querySelector(".item-lender__name").innerText;
   } else {
-    lenderName = "lender Name: ";
+    lenderName = " ";
   }
 
   const button = document
@@ -73,23 +101,21 @@ async function scrapeData() {
   } else {
   }
 
- 
   await wait(1000);
 
   var phoneNumber = "";
   if (document.querySelector(".item-lender__contact")) {
     if (button) {
-      phoneNumber =
-        "PhoneNumber: " +
-        document.querySelector(".item-lender__contact").innerText;
+      phoneNumber = document.querySelector(".item-lender__contact").innerText;
     } else {
       phoneNumber = "";
     }
   } else {
-    phoneNumber = "Phone Number: ";
+    phoneNumber = "";
   }
 
-  let imageName = "";
+  //let imageName = "";
+  let imageName = [];
 
   const tempElement = document.createElement("div");
   tempElement.innerHTML = document.querySelector("body").innerHTML;
@@ -101,7 +127,8 @@ async function scrapeData() {
   );
 
   for (let i of imageSrcs) {
-    imageName += "\n\n" + i;
+    //imageName += "\n\n" + i;
+    imageName.push(i);
   }
 
   var data = "";
@@ -110,13 +137,13 @@ async function scrapeData() {
     "\n\n" +
     headingName +
     "\n\n" +
-    condition +
+    condition.replace("Condition:","") +
     "\n\n" +
-    age +
+    age.replace("Age:","") +
     "\n\n" +
     description +
     "\n\n" +
-    rate +
+    rate.replace("Lend for:","") +
     "\n\n" +
     lenderName +
     "\n\n" +
@@ -128,9 +155,48 @@ async function scrapeData() {
   const textFilename = "text_content.txt";
   downloadText(data, textFilename);
 
+  const url = "http://localhost:8080/product/createProductScrap";
+  const payload = {
+    category: "string",
+    subCategory: "string",
+    categoryType: "string",
+    name: "string",
+    conditionValue: "string",
+    age: 0,
+    description: "string",
+    preExistingDefects: "string",
+    lendRate: "string",
+    userId: 5,
+    media: ["string"],
+    availabilityStatus: "AVAILABLE",
+    verificationStatus: "VERIFIED",
+    source: "string",
+    userName: "string",
+    mobileNumber: "string",
+  };
 
-  const data1 = "Data Scraped";
-  chrome.runtime.sendMessage({ message: "data_extracted", data: data1 });
+  // const payload = {
+  //   category: item.split("/")[0],
+  //   subCategory: item.split("/")[1],
+  //   categoryType: item.split("/")[2],
+  //   name: headingName,
+  //   conditionValue: replace("Condition:",""),
+  //   age: age.replace("Age:",""),
+  //   description: description,
+  //   preExistingDefects: "",
+  //   lendRate: rate.replace("Lend for:",""),
+  //   userId: 0,
+  //   media: imageName,
+  //   availabilityStatus: "",
+  //   verificationStatus: "",
+  //   source: "",
+  //   userName: lenderName,
+  //   mobileNumber: phoneNumber.replace("Send message", ""),
+  // };
+  sendPostRequest(url, payload);
+
+  // const data1 = "Data Scraped";
+  // chrome.runtime.sendMessage({ message: "data_extracted", data: data1 });
 }
 
 // Listen for messages from the background script
